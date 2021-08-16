9955f1332a714c943adadb6468fbb66cc588a9f2
victorekeleme <74677661+victorekeleme@users.noreply.github.com>
2021-08-15 20:54:11 -0400

    like button

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hb!7gmt=e6!1gf(pwtqe#-#f573$^5bs+8t*-=kper#2e_qn7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
