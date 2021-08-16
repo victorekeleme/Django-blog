98ee37c424a61fd1cd833f448c50478c4a01e953
victorekeleme <74677661+victorekeleme@users.noreply.github.com>
2021-08-15 21:44:54 -0400

    added postgresDB

--- website/settings.py
+++ website/settings.py
@@ -86,10 +86,16 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frendzydb',
+        'USER': 'postgres',
+        'PASSWORD': 'paynen12',
+        'HOST': 'localhost'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
